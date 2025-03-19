d4454e3b4f0daeb2e36c309c6c83b880ce92c3fe
margot-inox <margot@inox.com>
2025-03-19 08:58:48 +0100

    adds event section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ---------- BURGER MENU ----------
 let burgerMenuIcon = document.querySelector('.to-open');
 let xMenuIcon = document.querySelector('.to-close');
 let openedBurgerMenu = document.querySelector('.opened-burger-menu');
@@ -17,9 +18,35 @@ menuLinks.forEach(link => {
 });
 
 
+// ---------- CUSTOM MOUSSE HERO HEADER ----------
 let customMouseLight = document.querySelector(".custom-mouse-light");
 let rect = customMouseLight.getBoundingClientRect();
 
 customMouseLight.addEventListener("mousemove", function (e) {
   e.target.style.background = `radial-gradient(circle at ${e.clientX - rect.x}px ${e.clientY - rect.y}px, transparent, #080526 300px, #080526 100%)`;
 });
+
+
+// ---------- COUNTDOWN ----------
+function updateCountdown() {
+  const targetDate = new Date(2025, 6, 19, 8, 0, 0);
+  const now = new Date();
+  const timeLeft = targetDate - now;
+  let daysDom = document.querySelector('.days');
+  let hoursDom = document.querySelector('.hours');
+  let minutesDom = document.querySelector('.minutes');
+  let secodnsDom = document.querySelector('.seconds');
+
+  const days = Math.floor(timeLeft / (1000*60*60*24));
+  const hours = Math.floor((timeLeft % (1000*60*60*24))/(1000*60*60));
+  const minutes = Math.floor((timeLeft % (1000*60*60))/(1000*60));
+  const seconds = Math.floor((timeLeft % (1000*60))/1000);
+
+  daysDom.innerHTML = days;
+  hoursDom.innerHTML = hours;
+  minutesDom.innerHTML = minutes;
+  secodnsDom.innerHTML = seconds;
+}
+
+updateCountdown();
+const countdownInterval = setInterval(updateCountdown, 1000);
